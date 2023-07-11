7aa1f4861b92aa225ea10e9b0ddac6aefd7a39b5
HARSH KUMAR <hknitu28@gmail.com>
2023-07-11 20:09:52 +0530

    My first committ

---
+++ Readme.md
@@ -0,0 +1,5 @@
+conda create -r env python=3.10 -y
+
+conda activate C:\Users\hknit\OneDrive\Desktop\PYTHON\sensor_project\env
+
+pip install -r requirment.txt
---
+++ setup.py
@@ -0,0 +1,9 @@
+from  setuptools import find_packages ,setup
+setup(
+    name="src",
+    version="0.0.1",
+    author="Harsh",
+    author_email="hknitu28@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
